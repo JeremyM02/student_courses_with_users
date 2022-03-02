d597df6fe074a5cb876b04108011c38eef77c9bd
JeremyM02 <zom7@fresnou.org>
2022-03-02 08:33:56 -0800

    Reached Slide 51

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,14 @@
-const {User, Student} = require('../models');
+const {User, Student, Staff} = require('../models');
 const md5 = require('md5');
 const passport = require('passport');
 //render student registration form
 module.exports.renderStudentRegistrationForm = function(req, res){
-    res.render('user/register');
+    res.render('user/registerStudent');
+};
+
+//render staff registration form
+module.exports.renderStaffRegistrationForm = function(req, res){
+    res.render('user/registerStaff');
 };
 
 //register student account
@@ -17,6 +22,21 @@ module.exports.registerStudent = async function(req, res){
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         grade_level: req.body.grade_level,
+        user_id: user.id
+    });
+    res.redirect('/courses')
+};
+
+//register staff account
+module.exports.registerStaff = async function(req, res){
+    const user = await User.create({
+        email: req.body.email,
+        password:md5(req.body.password),
+        role: 'staff'
+    });
+    await Staff.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
         user_id: user.id
     });
     res.redirect('/courses')
@@ -37,3 +57,9 @@ module.exports.login = passport.authenticate('local', {
     failureRedirect: '/login',
     failureMessage: true
 });
+
+//logout
+module.exports.logout = function(req, res){
+    req.logout();
+    res.redirect('/login');
+};
